2a11d5834cd02de47279fb242c9b914912870442
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-03-22 01:07:57 -0400

    Starting React

---
+++ app.js
@@ -0,0 +1,28 @@
+// ReactDOM.render(<h1>Hello, everyone! My name is Orlando Yes my name is</h1>, document.getElementById("root"))
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+// console.log(page)
+
+// ReactDOM.render (
+//     page,
+//     document.getElementById("root")
+// )
+
+
+ReactDOM.render (
+    <div className="nav">
+        <button>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </button>
+    </div>
+)
