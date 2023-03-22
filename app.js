9b84084d8483c97b12a832f9ed196983ddaa8fdd
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-03-22 01:15:41 -0400

    Importing React

--- app.js
+++ app.js
@@ -14,15 +14,17 @@
 //     document.getElementById("root")
 // )
 
+import React from "react"
 
-ReactDOM.render (
-    <div className="nav">
-        <button>
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </button>
-    </div>
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
+
+ReactDOM.render(navbar, document.getElementById("root"))
